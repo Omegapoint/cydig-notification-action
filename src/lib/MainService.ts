57e50096aec972e2e15a893148d7dd4cfd3a8301
Linnéa Oxenwaldt <linnea.oxenwaldt@omegapoint.se>
2025-06-18 16:06:13 +0200

    change how we fetch variables that are needed

--- src/lib/MainService.ts
+++ src/lib/MainService.ts
@@ -2,7 +2,6 @@ import { CyDigConfig } from './model/CyDigConfig';
 import { getContentOfFile } from './JsonService';
 import { SlackService } from './SlackService';
 import { NotificationService } from './NotificationService';
-import config from '../config.json';
 import { ComplianceHistory } from './model/ComplianceHistory';
 import * as core from '@actions/core';
 
@@ -14,7 +13,6 @@ export class MainService {
         const slackAccessToken: string = core.getInput('slackAccessToken', {
             required: true,
         });
-        let dashboardUrl: string = config.dashboardUrl;
 
         const cydigConfig: CyDigConfig = getContentOfFile(cydigConfigPath);
 
@@ -24,10 +22,12 @@ export class MainService {
         if (!cydigConfig.communicationTool.slack.channelName) {
             throw new Error('Missing slack channel name in config');
         }
-        if (!dashboardUrl) {
+
+        let dashboardUrl: string;
+        if (!process.env?.dashboardUrl) {
             dashboardUrl = "https://cydig.omegapoint.cloud/dashboard";
-            core.info('No dashboard URL provided, using prod: ' + dashboardUrl);
         }
+        dashboardUrl = process.env.dashboardUrl as string;
 
         const notificationService: NotificationService = new NotificationService();
         const slackService: SlackService = new SlackService(slackAccessToken);
@@ -39,7 +39,7 @@ export class MainService {
         const shouldSendAlert: boolean = complianceHistory.shouldSendAlert();
         core.info(`Should send slack alert: ${shouldSendAlert}`);
 
-        if (shouldSendAlert) {        
+        if (shouldSendAlert) {
             await slackService.sendToSlack(
                 complianceHistory,
                 cydigConfig.communicationTool.slack.channelName,
