5661d674451b00f3db2c07d13c08495a4b3734ec
Linnéa Oxenwaldt <linnea.oxenwaldt@omegapoint.se>
2025-11-27 10:18:54 +0100

    adding lint and format to pipeline

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export async function run(): Promise<void> {
     try {
         await new MainService().run();
         // Success is implicit if no error is thrown
-    } catch (error: any) {
+    } catch (error) {
         core.error(error.message);
         core.setFailed('Failed to run action');
     }
